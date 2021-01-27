156a5ddce2b43d6c77e269e5b7c75bf6c88f89ea
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-01-27 13:42:50 +0100

    SearchBar component totally refactored with Hooks. Running properly

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,29 @@
 import React, { useState } from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ onFormSubmit }) => {
     const [term, setTerm] = useState('');
+    
+    const onSubmit = event => {
+        event.preventDefault();
 
-    onInputChange = event => {
-        setTerm(event.target.value)
+        onFormSubmit(term);
     };
     
-    onFormSubmit = event => {
-        event.preventDefault();
-        this.props.onFormSubmit(this.state.term)
-    };
-}:
+    return (
+        <div className="search-bar ui segment">
+            <form onSubmit={onSubmit} className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input 
+                    type="text" 
+                    value={term} 
+                    onChange={(event) => setTerm(event.target.value)}
+                    />
+                </div>
+            </form>
+        </div>
+    );
+};
 
-class SearchBar extends React.Component {
-
-
-    render() {
-        return (
-            <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input 
-                        type="text" 
-                        value={this.state.term} 
-                        onChange={this.onInputChange}
-                        />
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
 
 export default SearchBar;
